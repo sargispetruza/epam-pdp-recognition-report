d5a8b3276078a64a4b6d65353425fe03c6f5ba7c
p0keT <p0keT@ukr.net>
2019-11-25 22:37:42 +0200

    Initial commit

---
+++ src/main/java/com/epam/pdp/recognitionreport/RecognitionReportApplication.java
@@ -0,0 +1,13 @@
+package com.epam.pdp.recognitionreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecognitionReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecognitionReportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/epam/pdp/recognitionreport/RecognitionReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.pdp.recognitionreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecognitionReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
